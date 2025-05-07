3c1916ffa1ee0eb0544da5f56b50ddb673480bfb
Ivan Timoshenko <ivantm.dev@gmail.com>
2025-05-07 10:58:25 +0300

    .

---
+++ src/app/calc-v1/components/calc/calc.component.ts
@@ -0,0 +1,41 @@
+import {Component} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-calc',
+  imports: [
+    FormsModule
+  ],
+  templateUrl: './calc.component.html',
+  styleUrl: './calc.component.scss'
+})
+export class CalcComponent {
+  public operations: string[] = ['+', '-', '*', '/']
+  public selectOperation: string = this.operations[0]
+
+  public first: number = 1
+  public second: number = 1
+  public result?: number
+
+  public showCalcResult() {
+    switch (this.selectOperation) {
+      case '+':
+        this.result = this.first + this.second
+        break
+      case '-':
+        this.result = this.first - this.second
+        break
+      case '*':
+        this.result = this.first * this.second
+        break
+      case '/':
+        this.result = this.first / this.second
+        break
+      default:
+        break
+    }
+  }
+}
+
+// [value]="first"  - только читать
+// [(ngModel)]="first"  - читать и изменять
---
+++ src/app/calc-v2/components/calc/calc.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calc',
+  imports: [],
+  templateUrl: './calc.component.html',
+  styleUrl: './calc.component.scss'
+})
+export class CalcComponent {
+
+}
